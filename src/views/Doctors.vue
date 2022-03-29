677adac77d175094f0b1483e1107805912c2ec75
seanpe11 <sean.m.s.pe@gmail.com>
2022-03-29 16:24:14 +0800

    unstable but added a lot of doctor shit

--- src/views/Doctors.vue
+++ src/views/Doctors.vue
@@ -1,9 +1,49 @@
 <script setup lang="ts">
 import { useLoadDoctor } from '../composables';
-
+import { ref, reactive } from 'vue'
 const preferences = {}
 const symptoms = {}
+const loading = ref(false)
 
-const {firstRecommendations, secondRecommendations, specRecommendations, diagnosis} = await useLoadDoctor(preferences, symptoms);
 
+const {
+    loaded, 
+    firstRecommendations, 
+    secondRecommendations, 
+    specRecommendations, 
+    diagnosis
+} = reactive(await useLoadDoctor(preferences, symptoms));
+
+loading.value = loaded
 </script>
+
+<template>
+    <div id="doctor-container" className='mt-4'>
+        <h3 className='mb-3'>Doctors that matches all your preferences and best specialization</h3>
+        
+        <div className="row mb-2 cursor-pointer hover-effect-grey rounded-3" key={index} onClick={() => showDoctorInfo(doctor)}>
+            <div className="col-1">
+            <img src={doctor.sex === "Male" ? DoctorMale : DoctorFemale} alt="doctor_pic" style={{width: '50px'}}/>
+            </div>
+            <div className='col-4 ps-4'><span className="d-flex h-100 align-items-center">Dr. {doctor.name}<span class="badge bg-primary ms-2">{doctor.isFirst ? "1st" : "2nd"}</span> </span></div>
+            <div className='col-3'><span className="d-flex h-100 align-items-center">{doctor.specialization}</span></div>
+            <div className='col-4'><span className="d-flex h-100 align-items-center float-end">{doctor.clinic_address}</span></div>
+        </div>
+    </div>
+    <div id="doctor-container" className='mt-4'>
+        <h3 className='mb-3'>Doctors that matches your location preference and best specialization</h3>
+        <div className="row mb-2 cursor-pointer hover-effect-grey rounded-3" onClick={() => showDoctorInfo(doctor)}>
+            <div className="col-1">
+                <img src={doctor.sex === "Male" ? DoctorMale : DoctorFemale} alt="doctor_pic" style={{width: '50px'}}/>
+            </div>
+            <div className='col-4 ps-4'><span className="d-flex h-100 align-items-center">Dr. {doctor.name}<span class="badge bg-primary ms-2">{doctor.isFirst ? "1st" : "2nd"}</span> </span></div>
+            <div className='col-3'><span className="d-flex h-100 align-items-center">{doctor.specialization}</span></div>
+            <div className='col-4'><span className="d-flex h-100 align-items-center float-end">{doctor.clinic_address}</span></div>
+        </div>
+    </div>
+    <div id="doctor-container" className='mt-4'>
+        <h3 className='mb-3'>Doctors based on best specialization only</h3>
+            <Doctors />
+        <button className="btn btn-primary" onClick={() => {setShowMore(true)}}>Show doctors</button>
+    </div>
+</template>
